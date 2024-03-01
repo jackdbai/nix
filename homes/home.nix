d91251d8f0c7ddfdce6fd0e90b1e50e7644da893
Jack <jack@jackdbai.dev>
2024-03-01 18:21:55 -0500

    Enable ssh on elle

--- homes/home.nix
+++ homes/home.nix
@@ -29,7 +29,6 @@
       "rebuild" = "sudo rm -rf /etc/nixos/* && sudo cp -r /home/jack/git/nix/* /etc/nixos/. && sudo nixos-rebuild switch";
       "regit" = "cd ~/git/nix && git pull && sudo rm -rf /etc/nixos/* && sudo cp -r /home/jack/git/nix/* /etc/nixos/. && sudo nixos-rebuild switch";
       "upgrade" = "sudo nixos-rebuild switch --upgrade";
-      "yolo" = "echo 'it worked!'";
     };
   };
 
