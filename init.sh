e422010341044de6b7e47a50769f9c942d0c64ec
Jack <jack@jackdbai.dev>
2024-07-23 00:33:22 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+### SET ENVIRONMENT VARIABLE MACHINE TO THE DESIRED HOSTNAME (also change it in configuration.nix)
+
 mkdir ~/git/
 curl -L "https://github.com/jackdbai/nix/tarball/master" | tar xz -C ~/git
 mv ~/git/jackdbai* ~/git/nix
-rm -r ~/git/nix/hosts/alita/*
-cp /etc/nixos/* ~/git/nix/hosts/alita/.
-mv ~/git/nix/hosts/alita/configuration.nix ~/git/nix/hosts/alita/default.nix
+rm -r ~/git/nix/hosts/$MACHINE/*
+cp /etc/nixos/* ~/git/nix/hosts/$MACHINE/.
+mv ~/git/nix/hosts/$MACHINE/configuration.nix ~/git/nix/hosts/$MACHINE/default.nix
 sudo rm -rf /etc/nixos/*
 sudo cp -r ~/git/nix/* /etc/nixos/.
 cd ~/git/nix
-sudo nixos-rebuild switch --flake .#alita
+sudo nixos-rebuild switch --flake .#$MACHINE
